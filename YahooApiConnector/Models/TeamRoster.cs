f53fdcdbf7eb894308512bafa6ce7f886ea4693e
ELowe15 <elowe@dal.com>
2025-11-08 15:33:20 -0400

    bugfix teamname showing null

--- YahooApiConnector/Models/TeamRoster.cs
+++ YahooApiConnector/Models/TeamRoster.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 public class TeamRoster
 {
     public string? TeamKey { get; set; }
-    public string? TeamName { get; set; }
     public string? ManagerName { get; set; }
     public List<Player>? Players { get; set; }
 }
